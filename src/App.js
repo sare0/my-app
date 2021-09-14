c8e04564ed0225bbd3d19e68bd253b794599e3e4
sare0 <76596994+sare0@users.noreply.github.com>
2021-09-15 02:30:40 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar"
+import './App.scss';
+import TourList from "./components/TourList";
+
+function App() {
+  return (<main>
+    <Navbar />
+    <TourList />
+    
+    </main>);
+}
+
+export default App;
